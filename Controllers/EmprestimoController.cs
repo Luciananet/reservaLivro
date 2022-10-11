3b679f873121e78e4127805b7deee9e091e65618
Luciana <lucianaluciellen46@gmail.com>
2022-10-10 23:04:12 -0300

    Atalização de devolução por ano

--- Controllers/EmprestimoController.cs
+++ Controllers/EmprestimoController.cs
@@ -22,6 +22,9 @@ namespace Biblioteca.Controllers
         [HttpPost]
         public IActionResult Cadastro(CadEmprestimoViewModel viewModel)
         {
+            if(!string.IsNullOrEmpty(viewModel.Emprestimo.NomeUsuario)){
+
+           
             EmprestimoService emprestimoService = new EmprestimoService();
             
             if(viewModel.Emprestimo.Id == 0)
@@ -33,7 +36,26 @@ namespace Biblioteca.Controllers
                 emprestimoService.Atualizar(viewModel.Emprestimo);
             }
             return RedirectToAction("Listagem");
+             }
+            else
+            {
+                ViewData["mensagem"]="Por favor ,Preencha todos os campos";
+          LivroService livroService = new LivroService();
+          EmprestimoService emprestimoService = new EmprestimoService();
+
+          CadEmprestimoViewModel cadModel = new CadEmprestimoViewModel();
+           
+           cadModel.Livros = livroService.ListarDisponiveis();
+
+           return View(cadModel);
+
         }
+            
+            
+        }
+        
+        
+        
 
         public IActionResult Listagem(string tipoFiltro, string filtro)
         {
